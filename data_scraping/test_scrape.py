f5d4dbdef6d0a3bcec884ccab618588b6fb1b911
Chad Thut <chadthut@gmail.com>
2021-02-07 11:59:11 -0800

    advanced player stat

--- data_scraping/test_scrape.py
+++ data_scraping/test_scrape.py
@@ -1,7 +1,7 @@
 from basketball_reference_web_scraper import client
 from basketball_reference_web_scraper.data import OutputType, Team
 print(client.season_schedule(season_end_year=2021))
-client.players_season_totals(
+client.players_advanced_season_totals(
     season_end_year=2018, 
     output_type=OutputType.CSV, 
     output_file_path="./2017_2018_player_season_totals.csv"
