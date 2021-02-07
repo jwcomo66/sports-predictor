8c5571fb62aaa1f51779e5a8d50998d9298eb109
Chad Thut <chadthut@gmail.com>
2021-02-07 11:45:10 -0800

    -

--- data_scraping/test_scrape.py
+++ data_scraping/test_scrape.py
@@ -1,2 +1,8 @@
 from basketball_reference_web_scraper import client
-print(client.season_schedule(season_end_year=2018))
+from basketball_reference_web_scraper.data import OutputType
+print(client.season_schedule(season_end_year=2021))
+client.players_season_totals(
+    season_end_year=2018, 
+    output_type=OutputType.CSV, 
+    output_file_path="./2017_2018_player_season_totals.csv"
+)
